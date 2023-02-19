5f6abcd5dc8e89cc4d157945bfb5547477c2f0a9
Kenny Sweet <redredacted@gmail.com>
2023-02-19 14:02:52 -0600

    - added tailwindcss lang server and updated packages

--- init.lua
+++ init.lua
@@ -252,7 +252,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'pyright', 'tsserver', 'sumneko_lua', 'gopls', 'astro' }
+local servers = { 'clangd', 'pyright', 'tsserver', 'sumneko_lua', 'gopls', 'astro', 'tailwindcss' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
