8ec45a07d907e10785bae4cc551bc0f6a58b49d1
Kenny Sweet <redredacted@users.noreply.github.com>
2023-08-06 13:15:58 -0500

    removed telescope fzf native

--- init.lua
+++ init.lua
@@ -102,10 +102,6 @@ require("lazy").setup({
     branch = "0.1.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-      },
       -- "nvim-tree/nvim-web-devicons",
       -- {
       --   "nvim-telescope/telescope-ui-select.nvim",
